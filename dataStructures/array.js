2edfd75653ec75b9bf513aae4720a40890c1c6c1
Miguel Johnson <miguelbjohnson@gmail.com>
2023-12-22 23:44:36 -0500

    adding some exploration

--- dataStructures/array.js
+++ dataStructures/array.js
@@ -8,30 +8,30 @@ const list1 = [0,1,2,3,4,"hello"]
 const array1 = new Uint8Array(10)
 const arrayBuffer = new ArrayBuffer(8)
     /*
-        ArrayBuffer will create an array of zeroes with a total byteLength of 8.
+        ArrayBuffer will create an array of zeroes with a total byteLength of 8. It is a contiguous piece of memory the arrays below allow you to create a view into that piece of memor
         Now we need to interpret this ArrayBuffer by passing it into a Uint8array, Uint16Array or Uint32Array
     */
 
 
 const array8 = new Uint8Array(arrayBuffer) 
     /**
-        This breaks our buffer into 8 (8)byte sections with each section, with index starting at 0
-        00 | 00 | 00 | 00 | 00 | 00 | 00 | 00
+        This creates a 8 byte buffer, with an int8 view of the buffer. this breaks the buffer into 8 bit units
+        0 , 0 , 0 , 0 , 0 , 0 , 0 , 0
      */
 const array16 = new Uint16Array(arrayBuffer)
     /**
-        This breaks our buffer into 4 (16)byte sections, with index starting at 0
-         00  00 | 00  00 | 00  00 | 00  00
+        This creates a 8 byte buffer, with an int16 view of the buffer, this breaks the buffer into 16 bit units
+         0 , 0 , 0 , 0
      */
 const array32 = new Uint32Array(arrayBuffer)  
     /**
-        This breaks our buffer into 2 (32)byte sections, with index starting at 0
-         00  00  00  00 | 00  00  00  00
+        This creates a 8 byte buffer, with an int32 view of the buffer, this breaks the buffer into 32 bit units
+         0 , 0
      */
 
 
 // Now let's access some of the elements within these arrays
-array8[0] = 5
+array8[0] = 5 // these assignments can be decimal or hexidecimcal values
 array16[0] = 5
 array32[0] = 5
 
